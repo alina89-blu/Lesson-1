d4964d68e71817d648f1a105bbac2a0f474513af
Alina Bludaru <alinocica.89@mail.ru>
2022-07-21 14:02:27 +0300

    Added files

---
+++ Lesson 1/Lesson 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        public static int CalculateSumm(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
